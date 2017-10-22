66354b06cccead0f0b7831579e6f0e7ce278238d
ryanbrown358 <ryanbrown358@gmail.com>
2017-10-22 02:27:01 -0700

    Updated

--- Models/Automobile.cs
+++ Models/Automobile.cs
@@ -2,6 +2,11 @@ namespace SPVT.Models
 {
     public class Automobile
     {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public string LicensePlate { get; set; }
         
     }
 }
