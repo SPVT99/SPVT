227b3d2ab555b85f84826050dd385735adefc89a
ryanbrown358 <ryanbrown358@gmail.com>
2017-10-23 20:15:06 -0700

    Corrected SQLite string

--- Startup.cs
+++ Startup.cs
@@ -13,27 +13,29 @@ namespace SPVT
 {
     public class Startup
     {
-        public void ConfigureServices(IServiceCollection services)
-        {
-           //Add framework Services ///
-            services.AddMvc();
+        // public void ConfigureServices(IServiceCollection services)
+        // {
+        //    //Add framework Services ///
+        //     services.AddMvc();
 
-            services.AddDbContext<SPVTDbContext>(options 
-                                                => options.UseSqlite("Data Source=SPVT,db"));
-        }
+        //     services.AddDbContext<SPVTDbContext>(options 
+        //                                         => options.UseSqlite("Data Source=SPVT,db"));
+        // }
 
-        //  public Startup(IConfiguration configurSation)
-        //  {
+        //   public Startup(IConfiguration configurSation)
+        // {
         //      Configuration = configuration;
-        //  }
+        // }
 
         // // public IConfiguration Configuration { get; }
 
-        // // // This method gets called by the runtime. Use this method to add services to the container.
-        // // public void ConfigureServices(IServiceCollection services)
-        // // {
-        //     services.AddMvc();
-        // }
+        // This method gets called by the runtime. Use this method to add services to the container.
+         public void ConfigureServices(IServiceCollection services)
+         {
+            services.AddMvc();
+        services.AddDbContext<SPVTDbContext>(options
+                                                => options.UseSqlite("Data Source=SPVT.db"));
+         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
